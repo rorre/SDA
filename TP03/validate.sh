1ddb317828f19e24a3c59ce60f329dcf80031252
Rendy Arya Kemal <renrror@gmail.com>
2022-10-09 20:39:09 +0700

    background

--- TP03/validate.sh
+++ TP03/validate.sh
@@ -3,5 +3,5 @@ mkdir -p tc/outputuser
 for i in {000..119}
 do
     echo "Running $i"
-    cat tc/input/input$i.txt | python tp03.py > tc/outputuser/output$i.txt
+    cat tc/input/input$i.txt | python tp03.py > tc/outputuser/output$i.txt &
 done
